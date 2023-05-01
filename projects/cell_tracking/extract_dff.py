d077bd1488f15cff3cfd86dcac1d614543264220
Zahra D <zahra.dhanerawala@gmail.com>
2023-05-01 11:06:04 -0500

    clustering updates

--- projects/cell_tracking/extract_dff.py
+++ projects/cell_tracking/extract_dff.py
@@ -50,7 +50,11 @@ fall = loadmat(fallpth)
 epoch = np.where(fall['changeRewLoc']>1)
 for ep in range(len(epoch[1])):
     print(ep)
-    epoch_start,epoch_stop = epoch[1][ep],epoch[1][ep+1]
+    try:
+        epoch_start,epoch_stop = epoch[1][ep],epoch[1][ep+1]
+    except:
+        epoch_start,epoch_stop = epoch[1][ep],len(fall['trialnum'][0]) #else set 
+        # to end of reocrding
     rewloc = np.unique(fall['changeRewLoc'])[ep+1]
     trials = max(max(fall['trialnum'][:, epoch_start:epoch_stop])) # total number of trials
     dff_av = []
@@ -82,22 +86,22 @@ for ep in range(len(epoch[1])):
 
     dff_av_arr = np.mean(np.array(dff_av),axis=0).T        
     #https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+    if ep==0: # only make clusteres for first epoch and apply to 2nd epoch...
+        Z_columns = scipy.cluster.hierarchy.linkage(dff_av_arr, method='centroid')
+        max_d = 5
 
-    Z_columns = scipy.cluster.hierarchy.linkage(dff_av_arr, method='centroid')
-    max_d = 7
-
-    fancy_dendrogram(
-        Z_columns,
-        truncate_mode='lastp',  # show only the last p merged clusters
-        p=12,  # show only the last p merged clusters
-        leaf_rotation=90.,
-        leaf_font_size=12.,
-        show_contracted=True,  # to get a distribution impression in truncated branches
-        max_d=max_d # max distance
-    )
-  
-    clusters = scipy.cluster.hierarchy.fcluster(Z_columns, max_d, 
-                                                criterion='distance')
+        fancy_dendrogram(
+            Z_columns,
+            truncate_mode='lastp',  # show only the last p merged clusters
+            p=12,  # show only the last p merged clusters
+            leaf_rotation=90.,
+            leaf_font_size=12.,
+            show_contracted=True,  # to get a distribution impression in truncated branches
+            max_d=max_d # max distance
+        )
+    
+        clusters = scipy.cluster.hierarchy.fcluster(Z_columns, max_d, 
+                                                    criterion='distance')
 
     bigmap = np.zeros_like(dff_av_arr)
     for cl in np.unique(clusters):
@@ -105,6 +109,7 @@ for ep in range(len(epoch[1])):
         cellids = np.array(range(dff_av_arr.shape[0]))[clusters==cl]
         plt.figure()
         sns.heatmap(heatmap, cmap='viridis',yticklabels=cellids)
+        sns.set(font_scale=0.3)
         plt.ylabel("# of cells")
         plt.xlabel("track length (cm)")
         plt.title(f"cluster {cl}")
