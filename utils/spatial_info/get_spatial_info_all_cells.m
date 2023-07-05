ee4fe77a38e00017ec546eee6ee669bbda1d87b2
Zahra D <zahra.dhanerawala@gmail.com>
2023-07-05 10:22:21 -0500

    organizing utils files

---
+++ utils/spatial_info/get_spatial_info_all_cells.m
@@ -0,0 +1,17 @@
+
+
+function cell_info = get_spatial_info_all_cells2(Fc3,position,Fs,nBins,track_length)
+%% Fc3 = dFF of all cells in N X T format where N - number of cells and T - time  
+% position - position of animal on track 
+% Fs - Frame rate of acquisition
+% nBins - number of bins in which you want to divide the track into 
+% track_length - Length of track
+
+%%
+
+nCells = size(Fc3,1); % zd fixed the order because the description did not match the input order of the matrix, 7/4/23
+for cell = 1:nCells
+    cell_info(cell) = get_spatial_info_per_cell2(Fc3(cell,:),position,Fs,nBins,track_length);
+end
+
+
