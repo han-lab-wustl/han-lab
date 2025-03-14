22d79ecf72fd0bf1b7091c0a5cae2dba00ab8e44
Zahra D <zahra.dhanerawala@gmail.com>
2025-03-14 16:57:32 -0500

    correct vs. incorrect

--- projects/pyr_reward/prerewcells_correct_v_incorrect.py
+++ projects/pyr_reward/prerewcells_correct_v_incorrect.py
@@ -11,14 +11,14 @@ from collections import Counter
 from itertools import combinations, chain
 import matplotlib.backends.backend_pdf, matplotlib as mpl
 mpl.rcParams['svg.fonttype'] = 'none'
-mpl.rcParams["xtick.major.size"] = 8
-mpl.rcParams["ytick.major.size"] = 8
+mpl.rcParams["xtick.major.size"]=10
+mpl.rcParams["ytick.major.size"]=10
 # plt.rc('font', size=16)          # controls default text sizes
 plt.rcParams["font.family"] = "Arial"
 sys.path.append(r'C:\Users\Han\Documents\MATLAB\han-lab') ## custom to your clone
 from placecell import make_tuning_curves_radians_by_trialtype, intersect_arrays
 from projects.opto.behavior.behavior import get_success_failure_trials
-from rewardcell import get_radian_position,reward_act_nearrew
+from rewardcell import get_radian_position,reward_act_prerew
 # import condition df
 conddf = pd.read_csv(r"Z:\condition_df\conddf_pyr_goal_cells.csv", index_col=None)
 savedst = r'C:\Users\Han\Box\neuro_phd_stuff\han_2023-\pyramidal_cell_paper'
@@ -26,10 +26,10 @@ savepth = os.path.join(savedst, 'near_rew.pdf')
 #%%
 goal_cm_window=20 # to search for rew cells
 pdf = matplotlib.backends.backend_pdf.PdfPages(savepth)
-saveddataset = rf'Z:\saved_datasets\radian_tuning_curves_nearreward_cell_bytrialtype_nopto_{goal_cm_window}cm_window.p'
+saveddataset = rf'Z:\saved_datasets\radian_tuning_curves_prereward_cell_bytrialtype_nopto_{goal_cm_window}cm_window.p'
 with open(saveddataset, "rb") as fp: #unpickle
     radian_alignment_saved = pickle.load(fp)
-# radian_alignment_saved = {} # overwrite
+# radian_alignment_saved = {} # o verwrite
 goal_cell_iind = []
 goal_cell_prop = []
 goal_cell_null = []
@@ -41,7 +41,7 @@ epoch_perm = []
 radian_alignment = {}
 lasttr=8 #  last trials
 bins=90
-saveto = rf'Z:\saved_datasets\radian_tuning_curves_nearreward_cell_bytrialtype_nopto_{goal_cm_window}cm_window.p'
+saveto = rf'Z:\saved_datasets\radian_tuning_curves_prereward_cell_bytrialtype_nopto_{goal_cm_window}cm_window.p'
 # iterate through all animals
 dfs = []
 for ii in range(len(conddf)):
@@ -51,7 +51,7 @@ for ii in range(len(conddf)):
         if animal=='e145' or animal=='e139': pln=2 
         else: pln=0
         params_pth = rf"Y:\analysis\fmats\{animal}\days\{animal}_day{day:03d}_plane{pln}_Fall.mat"
-        df=reward_act_nearrew(ii,params_pth,\
+        df=reward_act_prerew(ii,params_pth,\
                 animal,day,bins,radian_alignment,radian_alignment_saved,goal_cm_window,
                 pdf,epoch_perm,goal_cell_iind,goal_cell_prop,num_epochs,goal_cell_null,pvals,
                 total_cells)
@@ -95,7 +95,7 @@ incor = bigdf.loc[(bigdf.trial_type=='incorrect'), 'mean_tc']
 t,pval = scipy.stats.ttest_rel(cor,incor)
 # statistical annotation       
 ii=0.5
-y=.12
+y=.10
 pshift=.01
 fs=30
 if pval < 0.001:
@@ -106,4 +106,4 @@ elif pval < 0.05:
         plt.text(ii, y, "*", ha='center', fontsize=fs)
 ax.text(ii, y+pshift, f'p={pval:.2g}',rotation=45,fontsize=12)
 
-ax.set_title('Post-reward cells',pad=40)
+ax.set_title('Pre-reward cells',pad=40)
